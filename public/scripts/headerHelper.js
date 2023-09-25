84e4c56b274a0bf774d469632eef5a024ff23dbb
clareysean <seanpclarey@gmail.com>
2023-09-25 14:28:56 -0400

    adjust nav sizing

--- public/scripts/headerHelper.js
+++ public/scripts/headerHelper.js
@@ -1,16 +1,20 @@
-const burger = document.querySelector('.burger');
-const navMenu = document.querySelector('.nav-menu');
-const navItems = [...document.querySelectorAll('.nav-item')]
+const burger = document.querySelector(".burger");
+const navMenu = document.querySelector(".nav-menu");
+const navItems = [...document.querySelectorAll(".nav-item")];
 
-burger.addEventListener('click', ()=> {
-    if(navItems.length<=2) navItems.forEach(item => item.classList.add('no-user-nav-menu'));
-    burger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-})
+burger.addEventListener("click", () => {
+  if (navItems.length <= 2)
+    navItems.forEach((item) => item.classList.add("no-user-nav-menu"));
+  burger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-const allNavLinks = [...document.querySelectorAll('.nav-link')]
+const allNavLinks = [...document.querySelectorAll(".nav-link")];
 
-allNavLinks.forEach(link => link.addEventListener('click', ()=> {
-    burger.classList.remove('active');
-    navMenu.classList.remove('active');
-}))
+allNavLinks.forEach((link) =>
+  link.addEventListener("click", () => {
+    burger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
