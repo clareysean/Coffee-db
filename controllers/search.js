2d339a889dab30e0d4523f3f67c7f276211bd7ae
clareysean <seanpclarey@gmail.com>
2023-09-27 14:52:32 -0400

    expand search to include roaster names

--- controllers/search.js
+++ controllers/search.js
@@ -1,41 +1,59 @@
-const Coffee = require('../models/coffee');
+const Coffee = require("../models/coffee");
 
-module.exports={
-    search
-}
+module.exports = {
+  search,
+};
 
 async function search(req, res) {
-    console.log(req.query);
-    const query = req.query.searchQuery;
-    const sanitizedQuery = query.replace(/\s+/g, '').toLowerCase();
-  
-    try {
-      const foundCoffees = await Coffee.aggregate([
-        {
-          "$match": {
-            "$expr": {
-              "$eq": [
-                {
-                  "$toLower": {
-                    "$replaceAll": {
-                      "input": "$name",
-                      "find": " ",
-                      "replacement": ""
-                    }
-                  }
-                },
-                sanitizedQuery
-              ]
-            }
-          }
-        }
-      ]).exec();
-  
-      res.render('coffee/results', { coffees: foundCoffees });
-    } catch (error) {
-      console.error('Error:', error);
-      res.status(500).json({ message: 'An error occurred' });
-    }
-  }
-    
+  console.log(req.query);
+  const query = req.query.searchQuery;
+  const sanitizedQuery = query.replace(/\s+/g, "").toLowerCase();
 
+  try {
+    const foundCoffees = await Coffee.aggregate([
+      {
+        $match: {
+          $or: [
+            {
+              $expr: {
+                $eq: [
+                  {
+                    $toLower: {
+                      $replaceAll: {
+                        input: "$name",
+                        find: " ",
+                        replacement: "",
+                      },
+                    },
+                  },
+                  sanitizedQuery,
+                ],
+              },
+            },
+            {
+              $expr: {
+                $eq: [
+                  {
+                    $replaceAll: {
+                      input: {
+                        $toLower: "$roaster",
+                      },
+                      find: " ",
+                      replacement: "",
+                    },
+                  },
+                  sanitizedQuery,
+                ],
+              },
+            },
+          ],
+        },
+      },
+    ]).exec();
+
+    res.render("coffee/results", { coffees: foundCoffees });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ message: "An error occurred" });
+  }
+}
